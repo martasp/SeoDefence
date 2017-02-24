c28afba7d9eef09b2278b8c992067a7a0ef2c5c6
martasp <martasp@inbox.lt>
2017-02-24 23:50:22 +0200

    Added fonts for menu

---
+++ TemplateMertumUnityGame/Assets/Game/scripts/services/Fader.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Fader : MonoBehaviour
+
+{
+    public Image myPanel;
+    public float fadeTime = 1f;
+    void Start()
+    {
+        Debug.Log("Pasiruoses fadinti");
+        Invoke("BlackToTransparent", 1);
+        PlayerPrefs.SetInt("score", 25);
+    }
+    public void BlackToTransparent()
+    {
+        //myPanel.color = new Color(0,0,0,1);
+        myPanel.CrossFadeAlpha(0f,fadeTime,true);
+    }
+    public void TransparentToBlak()
+    {
+       // myPanel.color = new Color(0, 0, 0, 0);
+        myPanel.CrossFadeAlpha(1f, fadeTime, true);
+    }
+
+}
