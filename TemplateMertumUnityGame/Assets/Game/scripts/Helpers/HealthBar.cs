c0e3b57e336da4f749a1a70a05b58b7753247d26
Eimantas Zubavičius <eimazuba@gmail.com>
2017-03-03 15:42:08 +0200

    Added health bar and a button to mock damage

---
+++ TemplateMertumUnityGame/Assets/Game/scripts/Helpers/HealthBar.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public int maxHP = 4;
+    public int currentHP = 4;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Damage()
+    {
+        Slider slider = GetComponent<Slider>();
+        currentHP -= 1;
+        slider.value = currentHP;
+        if (currentHP == 0)
+        {
+            Debug.Log("YOU ARE DEAD! Reviving...");
+            currentHP = maxHP;
+            slider.value = maxHP;
+        }
+        else Debug.Log("STILL OK");
+
+    }
+}
