ca429145cd77b296a9921b1f0d02b2dd4d4a2f0b
Martynas Pocius <martasp3289@gmail.com>
2017-03-17 22:38:53 +0200

    Added hp to enemys now enemies can die !

--- TemplateMertumUnityGame/Assets/Game/scripts/Game/Walk.cs
+++ TemplateMertumUnityGame/Assets/Game/scripts/Game/Walk.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour {
+public class Walk : MonoBehaviour {
 
     public bool IsMoving = false;
     public int  stopcount = 0;
---
+++ TemplateMertumUnityGame/Assets/Game/scripts/Game/hp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hp : MonoBehaviour {
+
+	// Use this for initialization\
+    public int maxHP;
+    public int CurrentHP;
+    private SpriteRenderer img;
+
+	void Start ()
+	{
+	    img = this.GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (CurrentHP<1)
+	    {
+	        var alpha = img.color.a;
+            var delta = img.color.a * Time.deltaTime*10;
+	        img.color = new Color(1f,1f,1f,alpha-delta);
+	    }
+	    if (img.color.a<0.01f)
+	    {
+	        Destroy(this.gameObject);
+	    }
+	}
+}
