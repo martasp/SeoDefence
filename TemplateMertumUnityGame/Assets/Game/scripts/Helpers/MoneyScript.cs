ecbde282aebfffe075d14a933a96fc0591e3b912
Eimantas Zubavičius <eimazuba@gmail.com>
2017-03-19 23:13:54 +0200

    Money counter and view added

---
+++ TemplateMertumUnityGame/Assets/Game/scripts/Helpers/MoneyScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyScript : MonoBehaviour {
+    public int money = 300;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Add(int amount)
+    {
+        Text text = GetComponent<Text>();
+        money += amount;
+        text.text = money.ToString();
+    }
+
+    public void Sub(int amount)
+    {
+        if (amount > money)
+            return;
+        Text text = GetComponent<Text>();
+        money -= amount;
+        text.text = money.ToString();
+    }
+}
