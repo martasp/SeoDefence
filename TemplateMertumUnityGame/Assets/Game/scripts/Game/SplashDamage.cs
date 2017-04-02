b0437129ed3a1b5dd0a1efbdbfcc2ebe227406e2
Martynas Pocius <martasp3289@gmail.com>
2017-04-03 00:07:02 +0300

    -Added explosion -wired up all components - fixed bugs

---
+++ TemplateMertumUnityGame/Assets/Game/scripts/Game/SplashDamage.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashDamage : MonoBehaviour {
+
+    public int splashDamage = 10; //damage of splash effect
+    public float radius = 2f; //radius of the splash damage effect
+    public string targetsTag = "enemy"; //deals damage to objects with this tag
+    public GameObject Explosion;
+
+    public void DealSplashDamage()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(targetsTag);
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector2.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy <= radius)
+            {
+                enemy.GetComponent<hp>().CurrentHP -= splashDamage;
+                Debug.Log("padaryta zala, liko gyvybiu: " + enemy.GetComponent<hp>().CurrentHP + ", atstumas: " + distanceToEnemy);
+            }
+
+        }
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        Debug.Log("trigerina bomba enemy:");
+        if (coll.gameObject.tag == targetsTag)
+            Debug.Log(coll.gameObject);
+            this.gameObject.GetComponent<SplashDamage>().DealSplashDamage();
+            var exp = Instantiate(Explosion,this.transform.position,new Quaternion());
+            Destroy(exp,5);
+            Destroy(this.gameObject);
+    }
+}
