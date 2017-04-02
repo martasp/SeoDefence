741bf50387d36bd8c952c7eaf460c6abd1758363
Martynas Pocius <martasp3289@gmail.com>
2017-04-03 00:27:16 +0300

    hotfix

--- TemplateMertumUnityGame/Assets/Game/scripts/Game/SplashDamage.cs
+++ TemplateMertumUnityGame/Assets/Game/scripts/Game/SplashDamage.cs
@@ -1,39 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class SplashDamage : MonoBehaviour {
+namespace Assets.Game.scripts.Game
+{
+    public class SplashDamage : MonoBehaviour {
 
-    public int splashDamage = 10; //damage of splash effect
-    public float radius = 2f; //radius of the splash damage effect
-    public string targetsTag = "enemy"; //deals damage to objects with this tag
-    public GameObject Explosion;
+        public int splashDamage = 10; //damage of splash effect
+        public float radius = 2f; //radius of the splash damage effect
+        public string targetsTag = "enemy"; //deals damage to objects with this tag
+        public GameObject Explosion;
 
-    public void DealSplashDamage()
-    {
-        GameObject[] enemies = GameObject.FindGameObjectsWithTag(targetsTag);
-
-        foreach (GameObject enemy in enemies)
+        public void DealSplashDamage()
         {
-            float distanceToEnemy = Vector2.Distance(transform.position, enemy.transform.position);
-            if (distanceToEnemy <= radius)
+            GameObject[] enemies = GameObject.FindGameObjectsWithTag(targetsTag);
+
+            foreach (GameObject enemy in enemies)
             {
-                enemy.GetComponent<hp>().CurrentHP -= splashDamage;
-                Debug.Log("padaryta zala, liko gyvybiu: " + enemy.GetComponent<hp>().CurrentHP + ", atstumas: " + distanceToEnemy);
+                float distanceToEnemy = Vector2.Distance(transform.position, enemy.transform.position);
+                if (distanceToEnemy <= radius)
+                {
+                    enemy.GetComponent<hp>().CurrentHP -= splashDamage;
+                    Debug.Log("padaryta zala, liko gyvybiu: " + enemy.GetComponent<hp>().CurrentHP + ", atstumas: " + distanceToEnemy);
+                }
+
             }
-
-        }
         
-    }
+        }
 
-    void OnCollisionEnter2D(Collision2D coll)
-    {
-        Debug.Log("trigerina bomba enemy:");
-        if (coll.gameObject.tag == targetsTag)
-            Debug.Log(coll.gameObject);
+        void OnCollisionEnter2D(Collision2D coll)
+        {
+            Debug.Log("trigerina bomba enemy:");
+            if (coll.gameObject.tag == targetsTag)
+                Debug.Log(coll.gameObject);
             this.gameObject.GetComponent<SplashDamage>().DealSplashDamage();
             var exp = Instantiate(Explosion,this.transform.position,new Quaternion());
             Destroy(exp,5);
             Destroy(this.gameObject);
+        }
     }
 }
