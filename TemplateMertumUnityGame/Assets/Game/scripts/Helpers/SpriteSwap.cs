6a5cf48532f8a8c54cbfc1be3b4e5fb13485afb6
Eimantas Zubavičius <eimantas.zubavicius@devbridge.com>
2017-05-20 16:57:28 +0300

    Tower fix and pause upgrade

--- TemplateMertumUnityGame/Assets/Game/scripts/Helpers/SpriteSwap.cs
+++ TemplateMertumUnityGame/Assets/Game/scripts/Helpers/SpriteSwap.cs
@@ -7,10 +7,15 @@ public class SpriteSwap : MonoBehaviour {
 	public Image img;
 	public  Sprite Sp;
 	private Sprite old;
+    public Text text;
+    public AudioSource music;
+
 	public void swap()
 	{
 		old = img.sprite;
 		img.sprite = Sp;
 		Sp = old;
+        text.enabled = !text.enabled;
+        music.mute = !music.mute;
 	}
 }
